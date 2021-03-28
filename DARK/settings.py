60ad67bafaa14c7e6b76e2ea3c6a3110da7c2c97
milosz <milosz.galas2@gmail.com>
2021-03-28 18:04:14 +0200

    login and register forms added

--- DARK/settings.py
+++ DARK/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tournament.apps.TournamentConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
