a71af380919fc01c141172bed513261c64dd8615
Przemysław Rożnawski <48837433+shuntrho@users.noreply.github.com>
2021-04-01 20:41:09 +0200

    Update tests.py

--- tournament/tests.py
+++ tournament/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+class SimpleTest(TestCase):
+
+    def test_assert_ok(self):
+        self.assertEqual(1, 1)
+
+    def test_assert_wrong(self):
+        self.assertEqual(0, 1)
+
 # Create your tests here.
