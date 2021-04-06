7db21ebb7a88328ffd2f095f24944bc8ebca8b1b
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2021-04-07 01:58:19 +0200

    Add datetime format in helptext

--- tournament/forms.py
+++ tournament/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class TournamentForm(forms.Form):
     name = forms.CharField(max_length=100, help_text="Required.")
-    start_date = forms.DateTimeField(help_text="Required.")
-    end_date = forms.DateTimeField(help_text="Required.")
+    start_date = forms.DateTimeField(help_text="Required. Format: yyyy-MM-dd hh:mm:ss")
+    end_date = forms.DateTimeField(help_text="Required. Format: yyyy-MM-dd hh:mm:ss")
