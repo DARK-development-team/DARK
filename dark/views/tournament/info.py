b165b01856413d3bcf1b673939bcee0aa701f224
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2021-04-28 20:42:24 +0200

    Edit TournamentInfoView

--- dark/views/tournament/info.py
+++ dark/views/tournament/info.py
@@ -15,23 +15,27 @@ class TournamentInfoView(DetailView):
     slug_field = 'id'
 
     def get(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect(reverse('tournament:all'))
-
         self.object = get_object_or_404(Tournament, id=self.kwargs['tournament'])
-
         tournament = self.object
-        user = request.user
-
-        if tournament.is_private and user not in tournament.participants.all():
-            return redirect(reverse('tournament:access', kwargs={'tournament': self.kwargs['tournament']}))
 
         context = super().get_context_data(object=tournament)
         context['rounds'] = TournamentRound.objects.filter(tournament=tournament)
         context['contestants'] = Team.objects.filter(tournament=tournament)
-        context['is_creator_viewing'] = tournament.creator == user
+        context['is_creator_viewing'] = False
         context['is_private'] = tournament.is_private
-        context['has_team'] = True if Tournament.objects.filter(team__teammember__user=user).filter(
-            id=tournament.id).count() != 0 else False
+        context['has_team'] = False
+
+        if request.user.is_authenticated:
+            user = request.user
+
+            if tournament.is_private and user not in tournament.participants.all():
+                return redirect(reverse('tournament:access', kwargs={'tournament': self.kwargs['tournament']}))
+
+            context['is_creator_viewing'] = tournament.creator == user
+            context['has_team'] = True if Tournament.objects.filter(team__teammember__user=user).filter(
+                id=tournament.id).count() != 0 else False
+
+        elif tournament.is_private:
+            return redirect(reverse('tournament:all'))
 
         return self.render_to_response(context)
