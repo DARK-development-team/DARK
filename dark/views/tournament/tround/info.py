1a86132ece546e293625cca7ee51ea9aba025f44
milosz <milosz.galas2@gmail.com>
2021-05-12 14:49:27 +0200

    added displaying of Tournament round requirements

--- dark/views/tournament/tround/info.py
+++ dark/views/tournament/tround/info.py
@@ -3,6 +3,7 @@ import os
 from django.shortcuts import render
 from django.views.generic import DetailView
 
+from dark.logic.platform_utils import get_platform_requirements
 from dark.models.tournament import TournamentRound
 from dark.logic import tround_execution
 
@@ -32,4 +33,5 @@ class TournamentRoundInfoView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['results'] = tround_execution.get_round_results(self.object)
+        context['requirements'] = get_platform_requirements(self.object.platform)
         return context
