29a07947a4eefa013d1d0d640c5878c3853581d9
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2021-05-12 16:35:32 +0200

    Restrict round run

--- dark/views/tournament/tround/info.py
+++ dark/views/tournament/tround/info.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 
 from dark.models.tournament import TournamentRound
@@ -13,6 +14,8 @@ class TournamentRoundInfoView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        round = get_object_or_404(TournamentRound, id=self.kwargs['tround'])
         #tround_utils.execute_round()
         #context['results'] = tround_utils.get_round_results()
+        context['is_creator_viewing'] = True if round.tournament.creator == self.request.user else False
         return context
