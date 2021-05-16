6f1790296d59d83df849f4e2e32966dbfa9535cd
Maria Polak <bloqdymary@gmail.com>
2021-05-16 14:20:01 +0200

    "Cancel" button in all forms

--- dark/views/tournament/tround/add.py
+++ dark/views/tournament/tround/add.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic import CreateView
 from django.urls import reverse
 
@@ -18,3 +19,12 @@ class AddTournamentRoundView(ForeignKeysMixin, CreateView):
             'tournament': self.object.tournament.id,
             'tround': self.object.id
         })
+
+    def post(self, request, *args, **kwargs):
+        if "cancel" in request.POST:
+            tournament_id = kwargs.get('tournament')
+            return redirect(reverse('tournament:info', kwargs={
+                'tournament': tournament_id
+            }))
+        else:
+            super(self)
