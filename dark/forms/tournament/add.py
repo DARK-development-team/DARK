d33c541021d883346bd893fb52e1d69fd42712bf
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2021-05-17 23:28:14 +0200

    Add restriction on adding tournaments
    Not it's not allowed to create a new tournament with a start date in the past. Moreover some start date end date constraints are added to avoid database exceptions.

--- dark/forms/tournament/add.py
+++ dark/forms/tournament/add.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm, DateTimeInput, IntegerField
+from django.forms import ModelForm, DateTimeInput, IntegerField, forms
+from django.utils import timezone
 
 from dark.models.tournament import Tournament
 
@@ -12,4 +13,17 @@ class AddTournamentForm(ModelForm):
             'end_date': DateTimeInput(attrs={'type': 'datetime-local'}),
         }
 
-    number_of_teams = IntegerField()
+    number_of_teams = IntegerField(min_value=0, max_value=100, initial=0)
+
+    def clean_start_date(self):
+        start_date = self.cleaned_data['start_date']
+        if start_date < timezone.now():
+            raise forms.ValidationError("The start date cannot be in the past!")
+        return start_date
+
+    def clean_end_date(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+        if end_date < start_date:
+            raise forms.ValidationError("The end date cannot be earlier than the start date!")
+        return end_date
