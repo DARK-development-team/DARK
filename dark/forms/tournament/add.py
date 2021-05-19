86b0d6cd9e25cf4e957b699f7a464971a5939ace
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2021-05-19 19:39:43 +0200

    Remove changes in forms

--- dark/forms/tournament/add.py
+++ dark/forms/tournament/add.py
@@ -1,6 +1,4 @@
-from django.core.exceptions import ValidationError
 from django.forms import ModelForm, DateTimeInput, IntegerField
-from django.utils import timezone
 
 from dark.models.tournament import Tournament
 
@@ -14,13 +12,4 @@ class AddTournamentForm(ModelForm):
             'end_date': DateTimeInput(attrs={'type': 'datetime-local'}),
         }
 
-    number_of_teams = IntegerField(min_value=0, max_value=100, initial=0)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        start_date = cleaned_data.get("start_date")
-        end_date = cleaned_data.get("end_date")
-        if start_date < timezone.now():
-            raise ValidationError("The start date cannot be in the past!")
-        if end_date < start_date:
-            raise ValidationError("The end date cannot be earlier than the start date!")
+    number_of_teams = IntegerField()
--- dark/forms/tournament/tround/add.py
+++ dark/forms/tournament/tround/add.py
@@ -1,8 +1,4 @@
-from datetime import datetime
-
-from django.core.exceptions import ValidationError
 from django.forms import ModelForm, DateTimeInput
-from django.utils import timezone
 
 from dark.models.tournament import TournamentRound
 
@@ -15,12 +11,3 @@ class AddTournamentRoundForm(ModelForm):
             'start_date': DateTimeInput(attrs={'type': 'datetime-local'}),
             'end_date': DateTimeInput(attrs={'type': 'datetime-local'}),
         }
-
-    def clean(self):
-        cleaned_data = super().clean()
-        start_date = cleaned_data.get("start_date")
-        end_date = cleaned_data.get("end_date")
-        if start_date < timezone.now():
-            raise ValidationError("The start date cannot be in the past!")
-        if end_date < start_date:
-            raise ValidationError("The end date cannot be earlier than the start date!")
