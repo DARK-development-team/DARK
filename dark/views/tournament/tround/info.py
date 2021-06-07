03293596c198ff8303533df2c50c09f1f93c9b1d
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2021-06-07 21:26:48 +0200

    Run round button hides after pressing

--- dark/views/tournament/tround/info.py
+++ dark/views/tournament/tround/info.py
@@ -1,6 +1,7 @@
 import threading
 
 from django.contrib import messages
+from django.shortcuts import render
 from django.views.generic import DetailView
 from dark.logic.platform_utils import get_platform_requirements
 from dark.logic import tround_execution
@@ -24,15 +25,21 @@ class TournamentRoundInfoView(DetailView):
         get_kwargs = {
             self.slug_field: kwargs[self.slug_url_kwarg]
         }
+
+        tround = TournamentRound.objects.get(**get_kwargs)
         threading.Thread(
-            target=lambda: tround_execution.execute_round(TournamentRound.objects.get(**get_kwargs))).start()
+            target=lambda: tround_execution.execute_round(tround)).start()
         messages.success(self.request, 'The round has been started, please wait for email notifications.')
 
-        return result
+        context = self.get_context_data()
+        context['status'] = 'running'
+        return render(request, self.template_name, context)
+
+        # return result
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['results'], context['log_file_path'], context['json_file_path'] =\
+        context['results'], context['log_file_path'], context['json_file_path'] = \
             tround_execution.get_round_results(self.object)
         context['requirements'] = get_platform_requirements(self.object.platform)
         return context
